5d11e13eb034d2cc2c1195f2b062eca5eef5ee91
诸云涛 <76689737+zyt-z@users.noreply.github.com>
2026-04-23 14:32:46 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 导航栏滚动
+    const navbar = document.getElementById('navbar');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 20) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
